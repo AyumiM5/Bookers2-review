bf74f8209228b58898c907adbff5ab81f92353b3
AyumiM5 <mmmiray.555@gmail.com>
2021-10-28 13:59:38 +0000

    [Add]favorites

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,12 @@ class Book < ApplicationRecord
   
   belongs_to :user
   has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   
   validates :title, presence: true, length: { minimum: 2, maximun: 20}
   validates :opinion, presence: true, length: { maximum: 50}
+  
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :index, :edit, :update]
   resources :books, only: [:new, :create, :show, :index, :edit, :update, :destroy] do
     resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
   end
   
 end
